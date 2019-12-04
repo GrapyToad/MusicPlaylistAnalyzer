38994c24018a7eb73553e6d60c4f2ab81e39fbc9
GrapyToad <46852746+GrapyToad@users.noreply.github.com>
2019-12-04 16:42:28 -0600

    Problem

--- MusicPlaylistAnalyzer/MusicPlaylistAnalyzer/Program.cs
+++ MusicPlaylistAnalyzer/MusicPlaylistAnalyzer/Program.cs
@@ -1,16 +1,64 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MusicPlaylistAnalyzer
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             Song[] songs = new Song[1500];
-            StreamReader sr = new StreamReader("/Users/gradytodd/HDD/Projects 2040/MusicPlaylistAnalyzer");
+            StreamReader sr = new StreamReader("SampleMusicPlaylist.txt");
+            StreamWriter sw = new StreamWriter("MusicPlaylistReport.txt");
             Console.WriteLine(sr.ReadLine());
+            int i = 0;
+            while (!(sr.EndOfStream))
+            {
+                songs[i] = ReadTabValue();
+                
+                i++;
+                
+            }
 
+            string report = "";
+
+            report += "Music Playlist Report\n";
+            //Linq
+            //How many songs recieved 200 or more plays?
+            report += ("How many songs recieved 200 or more plays?");
+            //Console.WriteLine("sorry");
+            IEnumerable<Song> twohundredormoreplays =
+                from Song in songs
+                where Song.plays >= 200
+                select Song;
+            foreach (Song song in twohundredormoreplays)
+            {
+                //Console.WriteLine("iterate me daddy");
+                report += song + "\n";
+            }
+            report += ("How many songs are in the playlist with the Genre of \"Alternative?\"\n");
+
+            var songswithgenrealternative = from Song in songs where Song.genre == "Alternative" select Song;
+            foreach (Song song in songswithgenrealternative)
+            {
+                report += song + "\n";
+            }
+
+
+            sw.WriteLine(report);
+
+            Song ReadTabValue()
+            {
+                string line = sr.ReadLine();
+                var values = line.Split('\t');
+                Song temp = new Song(values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7]);
+                return temp;
+            }
         }
+
+        
     }
 }
--- MusicPlaylistAnalyzer/MusicPlaylistAnalyzer/Song.cs
+++ MusicPlaylistAnalyzer/MusicPlaylistAnalyzer/Song.cs
@@ -3,17 +3,25 @@ namespace MusicPlaylistAnalyzer
 {
     public class Song
     {
-        string songName;
-        string artistName;
-        string album;
-        string genre;
-        int size;
-        int time;
-        int year;
-        int plays;
+        public string songName;
+        public string artistName;
+        public string album;
+        public string genre;
+        public int size;
+        public int time;
+        public int year;
+        public int plays;
 
-        public Song()
+        public Song(string sn, string an, string al, string ge, string si, string ti, string ye, string pl)
         {
+            this.songName = sn;
+            this.artistName = an;
+            this.album = al;
+            this.genre = ge;
+            this.size = Convert.ToInt32(si);
+            this.time = Convert.ToInt32(ti);
+            this.year = Convert.ToInt32(ye);
+            this.year = Convert.ToInt32(pl);
         }
         override public string ToString()
         {
